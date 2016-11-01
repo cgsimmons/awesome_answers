d8052af1ed06b4a929f6429db70b86cd3a766710
cgsimmons <chris.simmons.alt@gmail.com>
2016-11-01 11:11:39 -0700

    Move vote methods to application helper

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -6,22 +6,35 @@ class VotesController < ApplicationController
     vote.question = question
 
     if vote.save
-      redirect_to question_path(question), notice: "Voted!"
+      redirect_to question_path(question)
     else
       redirect_to question_path(question), alert: vote.error_description
     end
   end
 
   def update
+    question = vote.question
+    if vote.update vote_params
+      redirect_to question_path(question)
+    else
+      redirect_to question_path(question), alert: vote.error_description
+    end
   end
 
   def destroy
+    question = vote.question
+    vote.destroy
+    redirect_to question_path(question)
   end
 
   private
 
   def vote_params
     params.require(:vote).permit(:is_up)
+  end
+
+  def vote
+    @vote ||= Vote.find params[:id]
   end
 
   def question
--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -1,2 +1,47 @@
 module AnswersHelper
+  def upvote_link(question)
+    vote = question.votes.find_by(user: current_user)
+    method = if vote.nil?
+                      :post
+                    elsif vote.is_up?
+                      :delete
+                    else
+                      :patch
+                    end
+
+    path = if vote.nil?
+      question_votes_path(question, vote: {is_up:true})
+    else
+      vote_path(vote, vote: {is_up: true})
+    end
+
+    link_to(
+     fa_icon("arrow-up"), path, method: method,
+     class: "vote-link #{vote&.is_up? ? 'vote-up': ''}"
+     )
+
+  end
+  def downvote_link(question)
+    vote = question.votes.find_by(user: current_user)
+    method = if vote.nil?
+                      :post
+                    elsif vote.is_down?
+                      :delete
+                    else
+                      :patch
+                    end
+
+    path = if vote.nil?
+      question_votes_path(question, vote: {is_up:false})
+    else
+      vote_path(vote, vote: {is_up: false})
+    end
+
+    link_to(
+     fa_icon("arrow-down"), path, method: method,
+     class: "vote-link #{vote&.is_down? ? 'vote-down': ''}"
+     )
+
+  end
+
 end
