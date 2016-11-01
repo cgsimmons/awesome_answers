fcca40edd2ff7242904d71b59b33358ab7e0b004
cgsimmons <chris.simmons.alt@gmail.com>
2016-11-01 11:34:04 -0700

    Add vote value display

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,5 +1,6 @@
 class VotesController < ApplicationController
-
+  before_action :authenticate_user
+  
   def create
     vote = Vote.new vote_params
     vote.user = current_user
--- app/models/question.rb
+++ app/models/question.rb
@@ -30,6 +30,10 @@ class Question < ApplicationRecord
     likes.find_by(user: user)
   end
 
+  def vote_value
+    votes.up.count - votes.down.count
+  end
+
   private
   def set_defaults
     self.view_count ||=0
