7e66a49dbdfb871d9481f75fcf0d61965e0d76a3
cgsimmons <chris.simmons.alt@gmail.com>
2016-11-01 10:22:32 -0700

    Add initiali votes functionality to view

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,30 @@
 class VotesController < ApplicationController
+
+  def create
+    vote = Vote.new vote_params
+    vote.user = current_user
+    vote.question = question
+
+    if vote.save
+      redirect_to question_path(question), notice: "Voted!"
+    else
+      redirect_to question_path(question), alert: vote.error_description
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def vote_params
+    params.require(:vote).permit(:is_up)
+  end
+
+  def question
+    @question ||= Question.find params[:question_id]
+  end
 end
