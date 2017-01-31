c5a09c7ecb7e3d9a05a6d5f46a2525bc78bec34d
cgsimmons <chris.simmons.alt@gmail.com>
2017-01-31 16:24:29 +0900

    changed keys comments

--- config/initializers/app_keys.example.rb
+++ config/initializers/app_keys.example.rb
@@ -1,9 +1,10 @@
 # copy this file and rename it to app_keys.rb and make sure to put the correct keys
-#api credentials
+#twitter
 ENV['TWITTER_CONSUMER_KEY'] = '???'
 ENV['TWITTER_CONSUMER_SECRET'] = ' ???'
-#email credentials
+#email
 ENV['EMAIL_USER_NAME'] = '???'
 ENV['EMAIL_PASSWORD'] = '???'
+#AWS
 ENV['AWS_ACCESS_KEY_ID'] = '???'
 ENV['AWS_SECRET_ACCESS_KEY'] = '???'
