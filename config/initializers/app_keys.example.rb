d2c4b57aebf42490896c7e6133d5af830600359e
cgsimmons <chris.simmons.alt@gmail.com>
2017-01-31 19:55:25 +0900

    comment out keys

--- config/initializers/app_keys.example.rb
+++ config/initializers/app_keys.example.rb
@@ -1,10 +1,10 @@
-# copy this file and rename it to app_keys.rb and make sure to put the correct keys
+# copy this file and rename it to app_keys.rb and make sure to put the correct keys and uncomment the lines with keys.
 #twitter
-ENV['TWITTER_CONSUMER_KEY'] = '???'
-ENV['TWITTER_CONSUMER_SECRET'] = ' ???'
+#ENV['TWITTER_CONSUMER_KEY'] = '???'
+#ENV['TWITTER_CONSUMER_SECRET'] = ' ???'
 #email
-ENV['EMAIL_USER_NAME'] = '???'
-ENV['EMAIL_PASSWORD'] = '???'
+#ENV['EMAIL_USER_NAME'] = '???'
+#ENV['EMAIL_PASSWORD'] = '???'
 #AWS
-ENV['AWS_ACCESS_KEY_ID'] = '???'
-ENV['AWS_SECRET_ACCESS_KEY'] = '???'
+#ENV['AWS_ACCESS_KEY_ID'] = '???'
+#ENV['AWS_SECRET_ACCESS_KEY'] = '???'
